9c2cf2ab7b03bae7d0120023cea2b2b5d11510dc
psf-compute <89492887+psf-compute@users.noreply.github.com>
2026-07-31 12:09:15 -0400

    Add package interface

---
+++ src/logic_counterexample_engine/__init__.py
@@ -0,0 +1,23 @@
+"""Tools for evaluating propositional logic and checking arguments."""
+
+from .expressions import (
+    Biconditional,
+    Conjunction,
+    Disjunction,
+    Expr,
+    Implication,
+    Negation,
+    TruthAssignment,
+    Var,
+)
+
+__all__ = [
+    "Biconditional",
+    "Conjunction",
+    "Disjunction",
+    "Expr",
+    "Implication",
+    "Negation",
+    "TruthAssignment",
+    "Var",
+]
